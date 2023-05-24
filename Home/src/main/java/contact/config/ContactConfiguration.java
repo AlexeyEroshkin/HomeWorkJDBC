743e770213de721dcfbb2de7dc224be512fb3ba4
Алексей <aeroshkin504@gmail.com>
2023-05-24 10:07:14 +0300

    Initial commit

---
+++ Home/src/main/java/contact/config/ContactConfiguration.java
@@ -0,0 +1,9 @@
+package contact.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("contact")
+public class ContactConfiguration {
+}
